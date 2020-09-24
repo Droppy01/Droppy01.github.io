b2b26d44148301105f8f65df02fc1c57bb2821ff
Aron <aron@dingweb.nl>
2020-09-24 11:30:18 +0200

    move section to js

---
+++ js/productcCompunent.js
@@ -0,0 +1,24 @@
+class productcCompunent {
+    constructor(product, id) {
+        this.name = product.name;
+        this.img = product.img;
+        this.price = product.price;
+        this.id = id;
+        this.createElement()
+    }
+    createElement() {
+        this.Element = document.createElement("section");
+        this.Element.innerHTML =
+`
+<img src="${this.img}">
+<h3>${this.name}</h3>
+<p>€${this.price}</p>
+<button class=\"button\" id=\"${this.id}\">details</button>
+`
+    }
+    getElement(){
+        return( this.Element);
+    }
+
+    
+}
